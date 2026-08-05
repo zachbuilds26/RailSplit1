cfc10bd35291a7f28255671eee6e5604f49ad142
zach <emmaolatunji760@gmail.com>
2026-08-05 01:54:34 +0100

    feat: update logo to RailSplit_logo_transparent.png

--- src/components/ui/railsplit-logo.tsx
+++ src/components/ui/railsplit-logo.tsx
@@ -14,7 +14,7 @@ export function RailsplitLogo({ href = "/", compact = false }: RailsplitLogoProp
         aria-hidden="true"
       >
         <Image
-          src="/railsplit-mark-20260802.png"
+          src="/RailSplit_logo_transparent.png"
           alt=""
           fill
           sizes={compact ? "32px" : "36px"}
